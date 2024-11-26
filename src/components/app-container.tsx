9572c86cc6a962d87709f4687c750e70a75e00b8
olewandowski1 <olewandowski@soldevelo.com>
2024-11-26 20:21:13 +0100

    build: init setup

---
+++ src/components/app-container.tsx
@@ -0,0 +1,11 @@
+import { App } from '@module/fe-core';
+import { TemplateComponent } from '@module/fe-template';
+
+export const AppContainer = () => {
+  return (
+    <>
+      <App />
+      <TemplateComponent />
+    </>
+  );
+};
