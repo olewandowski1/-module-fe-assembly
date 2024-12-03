54645f0e8ccd92fc3e29f2890be73730242da88b
olewandowski1 <olewandowski@soldevelo.com>
2024-12-03 21:40:11 +0100

    build: add tailwind and shadcn compatibility

--- src/components/app-container.tsx
+++ src/components/app-container.tsx
@@ -1,11 +1,9 @@
 import { App } from '@module/fe-core';
-import { TemplateComponent } from '@module/fe-template';
 
 export const AppContainer = () => {
   return (
-    <>
+    <div>
       <App />
-      <TemplateComponent />
-    </>
+    </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
+// Import global styles
+import '@node_modules/@module/fe-core/src/styles/globals.css';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { AppContainer } from './components/app-container';
+import { AppContainer } from '@/components/app-container';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
