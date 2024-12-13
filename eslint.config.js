1dc95aa2e8e02414718b169a9dbd564dcb84ac22
olewandowski1 <olewandowski@soldevelo.com>
2024-12-13 22:46:07 +0100

    feat: adjust eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -19,10 +19,9 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['off'],
+      '@typescript-eslint/no-empty-object-type': ['off'],
+      '@typescript-eslint/no-explicit-any': ['off'],
     },
   }
 );
